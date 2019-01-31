7e611182f81ea7a9916660ef70b9197a1be6ce93
root <root@doorlock-voordeur.localdomain>
2019-01-31 15:15:53 +0100

    improved detection of false positive or glitches in GPIO / cabling

--- Button.py
+++ Button.py
@@ -20,7 +20,6 @@ class Button:
         '''setup an button and a callback function for opening a solenoid'''
         logger.info('init {:s} using name {:s} on gpio pin {:s}.'.format(self.__class__.__name__, name, gpio_pin))
 
-        self.time = 1.8
         self.name = name # used in logfile
         self.gpio_pin = str(gpio_pin)
         self.solenoid = solenoid
@@ -36,22 +35,22 @@ class Button:
 
     def cleanup(self):
         '''cleanup single GPIO pin: remove event detect and set to GPIO.IN with GPIO.PUD_OFF'''
-
         logger.debug('cleanup ' + self.__class__.__name__+ ': cleanup single GPIO pin ' + self.gpio_pin)
 	GPIO.remove_event_detect(self.gpio_pin)
         GPIO.setup(self.gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
 
 
     def my_callback(self, channel):
-	logger.info('{:s} {:s} pressed .'.format(self.__class__.__name__, self.name))
+	'''callback function for event_detect, called when button is pressed.'''
 
-	if GPIO.input(self.gpio_pin) == 1:
-		# falss positive 
-		logger.debug('{:s} {:s} False positive detected (input High).'.format(self.__class__.__name__, self.name))
-	else:
-		logger.debug('{:s} {:s} (input Low) .'.format(self.__class__.__name__, self.name))
+	if GPIO.input(channel) == 0:
+		# prevent acting on glitches in the GPIO or cabling, proceed if the input is still low
+		logger.info('{:s} {:s} pressed .'.format(self.__class__.__name__, self.name))
 
 		if self.solenoid is not None:
 			self.solenoid.open()
 
+	else:
+		# for those who are curious we will log false positives in our debug log
+		logger.debug('{:s} {:s} False positive detected (input High).'.format(self.__class__.__name__, self.name))
 
