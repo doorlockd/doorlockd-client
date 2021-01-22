2a313fec8ce9711087103c179a7585ef01318a06
Diederik Hamelink <diederik@wie-niet.nl>
2021-01-22 14:57:41 +0100

    fix random rfid.hwid

--- src/rest_api_lib/rest_api_orator.py
+++ src/rest_api_lib/rest_api_orator.py
@@ -26,6 +26,7 @@ class RestApiOrator(RestApi):
 			new = self._orator_model.create(item)
 
 		except ModelError as e:
+			print("DEBUG ModelError: ",e, typeof(e))
 			# ModelError is already a dict conform our json api stadard
 			raise ApiErrorRespons(e, 400)
 
