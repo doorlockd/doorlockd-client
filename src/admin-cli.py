ee0f74d7f627f65bf6ccb6c2e6e12da413f80871
Diederik Hamelink <diederik@wie-niet.nl>
2021-02-04 11:18:49 +0100

    command: Remove all hwid checksum bytes in db

--- src/admin-cli.py
+++ src/admin-cli.py
@@ -5,6 +5,7 @@ from cleo import Application
 # import Commands 
 import libs.cli.UsersCommand as Users
 import libs.cli.HelperCommand as Helper
+import libs.cli.FixCommand as Fix
 
 # init application
 application = Application()
@@ -14,6 +15,8 @@ application.add(Users.CreateCommand())
 application.add(Users.PasswdCommand())
 application.add(Users.ListCommand())
 application.add(Helper.GenSecretCommand())
+# application.add(Fix.FixRemoveChecksumByteCommand())
+Fix.addAllTo(application)
 
 # run application
 if __name__ == '__main__':
---
+++ src/libs/cli/FixCommand.py
@@ -0,0 +1,65 @@
+from cleo import Command
+
+import sys
+sys.path.append("..")
+
+# import models , use connect_db to initialize the db
+from models import *
+
+def connect_db():
+	# init db with correct config and env.
+	from libs.data_container import data_container as dc
+	dc.config_overwrite = {'doorlockd': {'enable_hardware': False, 'enable_webserver': False,}}
+	from app import db
+
+
+def addAllTo(application):
+	"""
+	add all commands in here to application
+	"""
+	application.add(FixRemoveChecksumByteCommand())
+	
+	
+
+class FixRemoveChecksumByteCommand(Command):
+	"""
+	Remove all hwid checksum bytes in db, for all *Tag like models (hwid "01:02:03:04:05" -> "01:02:03:04")
+
+	fix:remove_checksum_byte 
+	"""
+
+	def handle(self):
+		connect_db() # connect db
+		
+		cols=['model','hwid old', 'hwid new', 'done']
+		
+		table = self.table()
+		table.set_headers(cols)		
+		
+		data = []
+
+		# all 'Tag' like models with .hwid attributes
+		for model in ['Tag', 'UnknownTag']:
+			# for all items 
+			for tag in globals()[model].all():
+				hwid_old = t.hwid
+			
+				# is hwid 5 bytes ("01:02:03:04:05")
+				if(len(hwid_old) == 14 ):
+					# remove checksum byte
+					hwid_new = hwid_old[0:11] # first 11 chars
+				
+					# update db:
+					t.hwid = hwid_new
+					done = t.save()
+
+					# display in table
+					data.append([model, hwid_old, hwid_new, str(done)])
+			
+		table.set_rows(data)
+		table.render()
+		
+
+		
+
+
