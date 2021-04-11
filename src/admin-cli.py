8d4d57e710bb2d2d6f2a3ceaa770733245b5ce9f
Diederik Hamelink <diederik@wie-niet.nl>
2021-04-11 14:08:31 +0200

    add: bash completions, see "Cleo" Documentation on how to implement

--- src/admin-cli.py
+++ src/admin-cli.py
@@ -8,7 +8,7 @@ import libs.cli.HelperCommand as Helper
 import libs.cli.FixCommand as Fix
 
 # init application
-application = Application()
+application = Application(complete=True)
 
 # add commands to application
 application.add(Users.CreateCommand())
