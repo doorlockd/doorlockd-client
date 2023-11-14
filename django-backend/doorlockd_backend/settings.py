1f467fa9ef77a43c8dd8eaf5ef1aca0cce58a1e5
Matthijs Kooijman <matthijs@stdin.nl>
2023-11-14 21:41:09 +0000

    Put static files in run directory
    This ensures the django manage.py collectstatic also only ever needs to
    write to files inside the run directory.

--- django-backend/doorlockd_backend/settings.py
+++ django-backend/doorlockd_backend/settings.py
@@ -130,7 +130,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "static/" # Basic configuration when using manage.py collectstatic
+STATIC_ROOT = "run/static/" # Basic configuration when using manage.py collectstatic
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
