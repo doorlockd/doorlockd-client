7c027abaeb57c00231ef857f0140d8e0eaa16759
Matthijs Kooijman <matthijs@stdin.nl>
2023-11-14 21:41:09 +0000

    Put sqlite db in run directory
    This ensures the django process only ever needs to write to files inside
    the run directory.

--- django-backend/doorlockd_backend/settings.py
+++ django-backend/doorlockd_backend/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'doorlockd_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'run' / 'db.sqlite3',
     }
 }
 
