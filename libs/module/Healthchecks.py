8aa3f53de852ab45f08ab3aa64246fc7bbb5476d
Diederik Hamelink <diederik@wie-niet.nl>
2025-02-06 14:01:48 +0100

    Adds improved Healthcheck, config events per endpoint
     [module.hc_uptime]
     type = "Healthchecks"
     url = "<hc url 1>
     endpoint.ping = ['sync_success']
     endpoint.log = ['sync_fail_log', 'app_exit']
     endpoint.start = ['app_startup_complete']
     endpoint.fail = ['app_abort']
    
     [module.hc_logs]
     type = "Healthchecks"
     url = "<hc url 2>
     endpoint.log = ['sync_fail_log', 'other_abnomrmal_event']
     endpoint.fail = ['app_abort']
     endpoint.ping = ['app_startup_complete'] # to make uptime happy (1 year intrval == restart atleast once a year ???)

--- libs/module/Healthchecks.py
+++ libs/module/Healthchecks.py
@@ -1,6 +1,7 @@
 import libs.Module as module
 from libs.data_container import data_container as dc
 import requests
+import traceback
 
 logger = dc.logger
 
@@ -16,6 +17,9 @@ class Healthchecks(module.BaseModule):
         # event
         self.url = config.get("url", None)
 
+        # endpoints[ {'signal': ['event_to_list_to', 'more_events', ..]}, ... ]
+        self.endpoint = config.get("endpoint", [])
+
         if not self.url:
             logger.warning(f"Heathcheck failed: no url configured.")
             return
@@ -28,17 +32,29 @@ class Healthchecks(module.BaseModule):
         #
         # connect event to our callback
         #
+        if not self.endpoint:
+            logger.warning(
+                f"Heathcheck has no endpoints configured! (for url '{self.url}')"
+            )
 
-        # libs/module/DjangoBackendRfidAuth.py events:
-        dc.e.subscribe("sync_success", lambda data: self.healthcheck_ping())
-        dc.e.subscribe("sync_fail_log", lambda data: self.healthcheck("log", data))
+        #
+        for signal in self.endpoint:
 
-        # libs/Module.py
-        dc.e.subscribe(
-            "app_startup_complete", lambda data: self.healthcheck("start", data)
-        )
-        dc.e.subscribe("app_abort", lambda data: self.healthcheck("fail", data))
-        dc.e.subscribe("app_exit", lambda data: self.healthcheck("log", data))
+            # register events
+            if self.endpoint[signal]:
+
+                # lambda callback using signal=signal to capture loop variable by value instead of reference.
+                callback_fn = lambda data, signal=signal: self.healthcheck(signal, data)
+                for event in self.endpoint[signal]:
+                    dc.e.subscribe(event, callback_fn)
+                    logger.info(
+                        f"Heathcheck connected event: '{event}' to  signal: '{signal}'"
+                    )
+
+            else:
+                logger.warning(
+                    f"Heathcheck 'endpoint.{signal}' has no events configured! (for url '{self.url}')"
+                )
 
     def setup(self):
         pass
@@ -52,28 +68,35 @@ class Healthchecks(module.BaseModule):
     def teardown(self):
         pass
 
-    def healthcheck_ping(self):
-        """send ping, only if dc.module.app_startup_complete is True."""
-        if dc.module.app_startup_complete:
-            self.healthcheck()
-        else:
-            # operation is not proper, ignore this ping.
-            logger.info(
-                f"Heathcheck ping ignored: app_startup_complete hasn't completed or abort/exit has started."
-            )
-
-    def healthcheck(self, signal="", data={}):
+    def healthcheck(self, signal="ping", data={}):
         """
         Send ping or other signal with data.
-        - signal see https://healthchecks.io/docs/http_api/
+        - signal 'ping' means no specific signal
+        see https://healthchecks.io/docs/http_api/
 
         - request-body will be made from:
             - data['mesg']
             - data['exception']
 
         """
-        url = f"{self.url}"
-        url += f"/{signal}"
+
+        # we also accept 'ping' as empty signal name , so it doesn't look ugly in our toml config
+        if signal == "ping":
+            signal = ""
+
+        #
+        # send ping, only if dc.module.app_startup_complete is True.
+        #
+        if signal == "":
+            if not dc.module.app_startup_complete:
+                # app is not opperating proper, ignore this ping.
+                logger.info(
+                    f"Heathcheck ping ignored: app_startup_complete hasn't completed or abort/exit has started."
+                )
+                return
+
+        # construct endpoint url
+        url = f"{self.url}/{signal}"
 
         try:
             # prepare message body with mesg and data:
@@ -83,11 +106,18 @@ class Healthchecks(module.BaseModule):
                 if key in data:
                     msg_body += f"{key}: {data[key]}\n"
 
+            if "exception" in data:
+                try:
+                    tb = "".join(traceback.format_exception(data["exception"]))
+                    msg_body += f"traceback:\n{tb}\n"
+                except Exception as e:
+                    msg_body += f"traceback: traceback failed ({e})\n"
+
             # do healthcheck request
             requests.post(
                 url, msg_body, headers={"User-Agent": dc.app_name_ver}, timeout=10
             )
-            logger.info(f"Heathcheck call: '{signal}', '{data}'")
+            logger.debug(f"Heathcheck call: '{signal}', '{data}'")
 
         except requests.RequestException as e:
             logger.warning(f"Heathcheck failed ({url}, '{data}'): {e}")
